3004b3e88248a8baf62c70ae2299b2f220766b6b
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2020-08-20 23:20:52 +0900

    マイグレーション用の冪等SQLスクリプトを出力して、SQLで実行するように変更

---
+++ AspNetCoreApiExample/Migrations/idempotent.sql
@@ -0,0 +1,493 @@
+CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(95) NOT NULL,
+    `ProductVersion` varchar(32) NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+);
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `AspNetRoles` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `Name` varchar(191) CHARACTER SET utf8mb4 NULL,
+        `NormalizedName` varchar(191) CHARACTER SET utf8mb4 NULL,
+        `ConcurrencyStamp` longtext CHARACTER SET utf8mb4 NULL,
+        CONSTRAINT `PK_AspNetRoles` PRIMARY KEY (`Id`)
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `AspNetUsers` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `UserName` varchar(191) CHARACTER SET utf8mb4 NULL,
+        `NormalizedUserName` varchar(191) CHARACTER SET utf8mb4 NULL,
+        `Email` varchar(191) CHARACTER SET utf8mb4 NULL,
+        `NormalizedEmail` varchar(191) CHARACTER SET utf8mb4 NULL,
+        `EmailConfirmed` tinyint(1) NOT NULL,
+        `PasswordHash` longtext CHARACTER SET utf8mb4 NULL,
+        `SecurityStamp` longtext CHARACTER SET utf8mb4 NULL,
+        `ConcurrencyStamp` longtext CHARACTER SET utf8mb4 NULL,
+        `PhoneNumber` longtext CHARACTER SET utf8mb4 NULL,
+        `PhoneNumberConfirmed` tinyint(1) NOT NULL,
+        `TwoFactorEnabled` tinyint(1) NOT NULL,
+        `LockoutEnd` datetime(6) NULL,
+        `LockoutEnabled` tinyint(1) NOT NULL,
+        `AccessFailedCount` int NOT NULL,
+        `LastLogin` datetime(6) NULL,
+        `CreatedAt` datetime(6) NULL,
+        `UpdatedAt` datetime(6) NULL,
+        CONSTRAINT `PK_AspNetUsers` PRIMARY KEY (`Id`)
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `AspNetRoleClaims` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `RoleId` int NOT NULL,
+        `ClaimType` longtext CHARACTER SET utf8mb4 NULL,
+        `ClaimValue` longtext CHARACTER SET utf8mb4 NULL,
+        CONSTRAINT `PK_AspNetRoleClaims` PRIMARY KEY (`Id`),
+        CONSTRAINT `FK_AspNetRoleClaims_AspNetRoles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `AspNetRoles` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `AspNetUserClaims` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `UserId` int NOT NULL,
+        `ClaimType` longtext CHARACTER SET utf8mb4 NULL,
+        `ClaimValue` longtext CHARACTER SET utf8mb4 NULL,
+        CONSTRAINT `PK_AspNetUserClaims` PRIMARY KEY (`Id`),
+        CONSTRAINT `FK_AspNetUserClaims_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `AspNetUserLogins` (
+        `LoginProvider` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+        `ProviderKey` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+        `ProviderDisplayName` longtext CHARACTER SET utf8mb4 NULL,
+        `UserId` int NOT NULL,
+        CONSTRAINT `PK_AspNetUserLogins` PRIMARY KEY (`LoginProvider`, `ProviderKey`),
+        CONSTRAINT `FK_AspNetUserLogins_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `AspNetUserRoles` (
+        `UserId` int NOT NULL,
+        `RoleId` int NOT NULL,
+        CONSTRAINT `PK_AspNetUserRoles` PRIMARY KEY (`UserId`, `RoleId`),
+        CONSTRAINT `FK_AspNetUserRoles_AspNetRoles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `AspNetRoles` (`Id`) ON DELETE CASCADE,
+        CONSTRAINT `FK_AspNetUserRoles_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `AspNetUserTokens` (
+        `UserId` int NOT NULL,
+        `LoginProvider` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+        `Name` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+        `Value` longtext CHARACTER SET utf8mb4 NULL,
+        CONSTRAINT `PK_AspNetUserTokens` PRIMARY KEY (`UserId`, `LoginProvider`, `Name`),
+        CONSTRAINT `FK_AspNetUserTokens_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `Blogs` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `Name` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+        `UserId` int NOT NULL,
+        `CreatedAt` datetime(6) NULL,
+        `UpdatedAt` datetime(6) NULL,
+        CONSTRAINT `PK_Blogs` PRIMARY KEY (`Id`),
+        CONSTRAINT `FK_Blogs_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `Articles` (
+        `Id` int NOT NULL AUTO_INCREMENT,
+        `BlogId` int NOT NULL,
+        `Subject` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+        `Body` longtext CHARACTER SET utf8mb4 NOT NULL,
+        `CreatedAt` datetime(6) NULL,
+        `UpdatedAt` datetime(6) NULL,
+        CONSTRAINT `PK_Articles` PRIMARY KEY (`Id`),
+        CONSTRAINT `FK_Articles_Blogs_BlogId` FOREIGN KEY (`BlogId`) REFERENCES `Blogs` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE TABLE `Tags` (
+        `ArticleId` int NOT NULL,
+        `Name` varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+        CONSTRAINT `PK_Tags` PRIMARY KEY (`ArticleId`, `Name`),
+        CONSTRAINT `FK_Tags_Articles_ArticleId` FOREIGN KEY (`ArticleId`) REFERENCES `Articles` (`Id`) ON DELETE CASCADE
+    );
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_Articles_BlogId` ON `Articles` (`BlogId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_Articles_CreatedAt` ON `Articles` (`CreatedAt`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_Articles_Subject` ON `Articles` (`Subject`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_AspNetRoleClaims_RoleId` ON `AspNetRoleClaims` (`RoleId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE UNIQUE INDEX `RoleNameIndex` ON `AspNetRoles` (`NormalizedName`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_AspNetUserClaims_UserId` ON `AspNetUserClaims` (`UserId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_AspNetUserLogins_UserId` ON `AspNetUserLogins` (`UserId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_AspNetUserRoles_RoleId` ON `AspNetUserRoles` (`RoleId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_AspNetUsers_CreatedAt` ON `AspNetUsers` (`CreatedAt`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_AspNetUsers_LastLogin` ON `AspNetUsers` (`LastLogin`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `EmailIndex` ON `AspNetUsers` (`NormalizedEmail`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE UNIQUE INDEX `UserNameIndex` ON `AspNetUsers` (`NormalizedUserName`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_Blogs_CreatedAt` ON `Blogs` (`CreatedAt`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_Blogs_Name` ON `Blogs` (`Name`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_Blogs_UserId` ON `Blogs` (`UserId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    CREATE INDEX `IX_Tags_Name_ArticleId` ON `Tags` (`Name`, `ArticleId`);
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
+
+
+DROP PROCEDURE IF EXISTS MigrationsScript;
+DELIMITER //
+CREATE PROCEDURE MigrationsScript()
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM `__EFMigrationsHistory` WHERE `MigrationId` = '20191201000001_InitialCreate') THEN
+
+    INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+    VALUES ('20191201000001_InitialCreate', '3.1.0');
+
+    END IF;
+END //
+DELIMITER ;
+CALL MigrationsScript();
+DROP PROCEDURE MigrationsScript;
