398dfcfdc2cffb05eb6168c5b25a29292fa7a31c
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2020-09-04 00:23:33 +0900

    パスワード強度の設定をappsettings.jsonに外出し

--- AspNetCoreApiExample/Startup.cs
+++ AspNetCoreApiExample/Startup.cs
@@ -86,16 +86,7 @@ namespace Honememo.AspNetCoreApiExample
             services.AddIdentity<User, IdentityRole<int>>()
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
-            services.Configure<IdentityOptions>(options =>
-            {
-                // パスワード強度の設定
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 1;
-                options.Password.RequiredUniqueChars = 1;
-            });
+            services.Configure<IdentityOptions>(options => this.Configuration.Bind("Identity", options));
 
             // DI設定
             services.AddScoped<IUnitOfWork>(x => x.GetRequiredService<AppDbContext>());
